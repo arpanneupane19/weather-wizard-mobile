504b1c8b9c7cd86065e977d77894d49ad7ecd9c8
Arpan Neupane <arpanneupane19@gmail.com>
2021-02-02 17:44:37 -0600

    design changes

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, ImageBackground, ScrollView, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, ImageBackground, ScrollView, Text, View, TextInput, TouchableOpacity, Touchable } from 'react-native';
 
 
 export default function App() {
@@ -19,9 +19,9 @@ export default function App() {
     }
 
     return (
-        <ImageBackground source={(typeof weatherData.main != 'undefined') ? (weatherData.main.temp > 50) ? require('./assets/warm-bg.png') : require('./assets/cold-bg.png') : require('./assets/cold-bg.png')} style={{ width: '100%', height: '100%' }}>
-            <ScrollView>
-            <View style={styles.container}>
+
+            <ScrollView style={styles.container}>
+            <View>
                 <View style={styles.main}>
                     <TextInput
                         placeholder='Enter City...'
@@ -39,96 +39,155 @@ export default function App() {
                             )
                         }}
                     />
-                    <Text style={styles.date}>{dateBuilder(new Date())}</Text>
+
 
                     {(typeof weatherData.main != 'undefined') ? (
-                        <View>
+                        <TouchableOpacity style={styles.weather} onPress={() => unit ? setUnit(false) : setUnit(true)} >
                             <Text style={styles.city}>{weatherData.name}, {weatherData.sys.country}</Text>
-
-                            <TouchableOpacity style={styles.tempInfo} onPress={() => unit ? setUnit(false) : setUnit(true)}>
-                                {unit ? <Text style={styles.temp}>{Math.round(weatherData.main.temp)}ºF</Text> : <Text style={styles.temp}>{Math.round((weatherData.main.temp - 32) * 5 / 9)}ºC</Text>}
-                                <Text style={styles.weather}>{weatherData.weather[0].main}</Text>
-                            </TouchableOpacity>
-                        </View>
+                            {unit ? <Text style={styles.temp}>{Math.round(weatherData.main.temp)}ºF</Text> : <Text style={styles.temp}>{Math.round((weatherData.main.temp - 32) * 5 / 9)}ºC</Text>}
+                            <Text style={styles.outside}>{weatherData.weather[0].main}</Text>
+                        </TouchableOpacity>
                     ) : (<></>)}
+
+                    <Text style={styles.date}>{dateBuilder(new Date())}</Text>
+
+                    <View style={styles.creditBox}>
+                        <Text style={styles.credit}>Weather Wizard developed by <Text style={styles.name}>Arpan Neupane</Text></Text>
+                    </View>
+
                 </View>
 
-                <Text style={styles.credit}>Weather Wizard developed by Arpan Neupane</Text>
+                
+
 
             </View>
             </ScrollView>
-        </ImageBackground>
+
 
     );
 }
 
+
 const styles = StyleSheet.create({
 
     container: {
-        fontFamily: 'Montserrat'
+        fontFamily: 'Helvetica Neue',
+        backgroundColor: '#f2f2f2',
     },
 
     main: {
-        marginTop: 66,
-        marginLeft: '10%',
-        marginRight: '10%',
-        width: '100%'
+        marginTop: 76,
+        width: '100%',
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
     },
 
     cityInput: {
         width: '80%',
         height: 54,
         padding: 15,
-        backgroundColor: 'rgba(255,255,255,0.8)',
+        backgroundColor: '#fff',
         color: '#000',
         fontWeight: '400',
         borderRadius: 7,
-        fontSize: 20
-    },
-
-    date: {
-        marginTop: 41,
         fontSize: 20,
-        marginBottom: 34,
-        lineHeight: 24,
-        color: '#fff'
-    },
-
-    city: {
-        fontSize: 20,
-        lineHeight: 24,
-        marginBottom: 41,
-        color: '#fff'
-    },
-
-    tempInfo: {
-        width: '80%',
-        height: 205,
-        borderRadius: 21,
-        backgroundColor: 'rgba(255,255,255,0.9)',
-        padding: 15,
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-evenly',
-        alignItems: 'center',
-        marginBottom: 40
-    },
-
-    temp: {
-        fontSize: 60
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.29,
+        shadowRadius: 4.65,
+        
+        elevation: 7,
     },
 
     weather: {
-        fontSize: 30
+        width: 231,
+        height: 231,
+        marginTop: 150,
+        padding: 45,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 9,
+        },
+        shadowOpacity: 0.48,
+        shadowRadius: 11.95,
+        elevation: 18,
+        borderRadius: 25,
+        backgroundColor: "#fff",
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-evenly'
+
+    },
+
+    city: {
+        fontWeight: '500',
+        fontSize: 20,
+        fontStyle: 'normal',
+        lineHeight: 24,
+        textAlign: 'center'
+    },
+
+    temp: {
+        fontStyle: 'normal',
+        fontSize: 40,
+        color: "#c60000",
+        textAlign: 'center',
+        marginTop: 16,
+        lineHeight: 48
+    },
+
+    outside: {
+        fontStyle: 'normal',
+        fontSize: 23,
+        textAlign: 'center',
+        marginTop: 10,
+        lineHeight: 27
+    },
+
+    date: {
+        fontSize: 23,
+        marginTop: 40,
+        textAlign: 'center'
+    },
+
+    creditBox: {
+        alignItems: 'center',
+        height: 148,
+        width: 319,
+        backgroundColor: '#FFF',
+        borderRadius: 9,
+        marginTop: 100,
+        padding: 15,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 9,
+        },
+        shadowOpacity: 0.48,
+        shadowRadius: 11.95,
+        elevation: 18,
+        borderRadius: 9,
+        backgroundColor: "#fff",
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center'
     },
 
     credit: {
-        fontSize: 20,
         textAlign: 'center',
-        color: '#fff',
-        marginLeft: 10,
-        marginRight: 10
+        fontSize: 23,
+    },
+
+    name: {
+        color: '#0085ff'
     }
+
 
 });
 
