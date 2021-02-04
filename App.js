beb881b5cf5b7b5c24b46c50c41549e538a5dbb9
Arpan Neupane <arpanneupane19@gmail.com>
2021-02-03 19:11:05 -0600

    minor design improvements/changes. added min & max temps. everything works

--- App.js
+++ App.js
@@ -5,64 +5,81 @@ import { StyleSheet, ImageBackground, ScrollView, Text, View, TextInput, Touchab
 
 export default function App() {
     const [weatherData, setWeatherData] = useState([{}])
-    const apiKey = '<API>'
+    const apiKey = '<API KEY>'
     const [city, setCity] = useState('');
     const [unit, setUnit] = useState(true);
 
     const dateBuilder = (d) => {
         let months = ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sep', "Oct", 'Nov', 'Dec'];
+        let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
+        let day = days[d.getDay()];
         let date = d.getDate();
         let month = months[d.getMonth()];
+        let year = d.getFullYear();
 
-        return `Today, ${date} ${month}`
+        return `${day} ${month} ${date}, ${year}`
+
     }
 
     return (
+        <ImageBackground source={(typeof weatherData.main != 'undefined') ? (weatherData.main.temp > 50) ? require('./assets/warm-bg.jpg') : require('./assets/cold-bg.jpg') : require('./assets/cold-bg.jpg')} style={{ width: '100%', height: '110%'}}>
+            <ScrollView>
+                <View style={styles.container}>
+                    <View style={styles.main}>
+                        <TextInput
+                            placeholder='Enter City...'
+                            style={styles.cityInput}
+                            onChangeText={text => setCity(text)}
+                            value={city}
+                            onSubmitEditing={() => {
+                                fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}`).then(
+                                    response => response.json()
+                                ).then(
+                                    data => {
+                                        setWeatherData(data);
+                                        setCity('')
+                                    }
+                                )
+                            }}
+                        />
 
-            <ScrollView style={styles.container}>
-            <View>
-                <View style={styles.main}>
-                    <TextInput
-                        placeholder='Enter City...'
-                        style={styles.cityInput}
-                        onChangeText={text => setCity(text)}
-                        value={city}
-                        onSubmitEditing={() => {
-                            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}`).then(
-                                response => response.json()
-                            ).then(
-                                data => {
-                                    setWeatherData(data);
-                                    setCity('')
-                                }
-                            )
-                        }}
-                    />
 
 
-                    {(typeof weatherData.main != 'undefined') ? (
-                        <TouchableOpacity style={styles.weather} onPress={() => unit ? setUnit(false) : setUnit(true)} >
-                            <Text style={styles.city}>{weatherData.name}, {weatherData.sys.country}</Text>
-                            {unit ? <Text style={styles.temp}>{Math.round(weatherData.main.temp)}ºF</Text> : <Text style={styles.temp}>{Math.round((weatherData.main.temp - 32) * 5 / 9)}ºC</Text>}
-                            <Text style={styles.outside}>{weatherData.weather[0].main}</Text>
-                        </TouchableOpacity>
-                    ) : (<></>)}
+                        {(typeof weatherData.main != 'undefined') ? (
+                            <View style={styles.weatherInfo}>
+                                <Text style={styles.city}>{weatherData.name}, {weatherData.sys.country}</Text>
+                                <Text style={styles.date}>{dateBuilder(new Date())}</Text>
+                                <TouchableOpacity style={styles.weatherBox} onPress={() => unit ? setUnit(false) : setUnit(true)}>
+                                    {unit ? <Text style={styles.temp}>{Math.round(weatherData.main.temp)}ºF</Text> : <Text style={styles.temp}>{Math.round((weatherData.main.temp - 32) * 5 / 9)}ºC</Text>}
+                                </TouchableOpacity>
+                                    {unit ? <Text style={styles.minMax}>Min: {Math.round(weatherData.main.temp_min)}ºF / Max: {Math.round(weatherData.main.temp_max)}ºF</Text> : <Text style={styles.minMax}>Min: {Math.round((weatherData.main.temp_min - 32) * 5/9)}ºC / Max: {Math.round((weatherData.main.temp_max - 32) * 5/9)}ºC</Text>}
+                                <Text style={styles.weather}>{weatherData.weather[0].main}</Text>
 
-                    <Text style={styles.date}>{dateBuilder(new Date())}</Text>
+                            </View>
+                        ) : (<>
+                                <View style={styles.weatherInfo}>
+                                    <Text style={styles.city}>Weather Wizard</Text>
+                                    <Text style={styles.date}>{dateBuilder(new Date())}</Text>
+                                </View>
 
-                    <View style={styles.creditBox}>
-                        <Text style={styles.credit}>Weather Wizard developed by <Text style={styles.name}>Arpan Neupane</Text></Text>
+                            </>)}
+
+                            {(weatherData.cod === '404') ? (
+                                <Text style={{marginTop: 50, color: '#fff', fontSize: 20, textAlign: 'center', marginRight: 20, marginLeft: 20}}>No city found. Maybe try just putting the city name?</Text>
+                            ): (<></>)}
+
+                            <View style={styles.credit}>
+                                <Text style={styles.myName}>Developed by <Text style={styles.fullName}>Arpan Neupane</Text></Text>
+                            </View>
+
+                        
                     </View>
 
+
                 </View>
-
-                
-
-
-            </View>
             </ScrollView>
-
+        </ImageBackground>
 
     );
 }
@@ -71,123 +88,97 @@ export default function App() {
 const styles = StyleSheet.create({
 
     container: {
-        fontFamily: 'Helvetica Neue',
-        backgroundColor: '#f2f2f2',
+
     },
 
     main: {
-        marginTop: 76,
+        marginTop: 66,
         width: '100%',
         flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center',
         alignItems: 'center'
     },
 
     cityInput: {
-        width: '80%',
+        width: '85%',
+        marginRight: '10%',
+        marginLeft: '10%',
         height: 54,
         padding: 15,
-        backgroundColor: '#fff',
+        backgroundColor: 'rgba(255,255,255,0.8)',
         color: '#000',
         fontWeight: '400',
         borderRadius: 7,
-        fontSize: 20,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 3,
-        },
-        shadowOpacity: 0.29,
-        shadowRadius: 4.65,
-        
-        elevation: 7,
+        fontSize: 20
     },
 
-    weather: {
-        width: 231,
-        height: 231,
-        marginTop: 150,
-        padding: 45,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 9,
-        },
-        shadowOpacity: 0.48,
-        shadowRadius: 11.95,
-        elevation: 18,
-        borderRadius: 25,
-        backgroundColor: "#fff",
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-evenly'
-
+    weatherInfo: {
+        marginTop: 100
     },
 
     city: {
-        fontWeight: '500',
+        color: '#fff',
+        fontSize: 32,
+        fontWeight: '300',
+        textAlign: 'center',
+    },
+
+    date: {
+        color: "#fff",
         fontSize: 20,
-        fontStyle: 'normal',
-        lineHeight: 24,
+        fontWeight: '300',
+        fontStyle: 'italic',
+        textAlign: 'center'
+    },
+
+    weatherBox: {
         textAlign: 'center'
     },
 
     temp: {
-        fontStyle: 'normal',
-        fontSize: 40,
-        color: "#c60000",
-        textAlign: 'center',
-        marginTop: 16,
-        lineHeight: 48
+        position: 'relative',
+        marginTop: 30,
+        marginBottom: 30,
+        backgroundColor: 'rgba(255,255,255,0.2)',
+        marginRight: 'auto',
+        marginLeft: 'auto',
+        color: "#fff",
+        paddingTop: 15,
+        paddingBottom: 15,
+        paddingLeft: 25,
+        paddingRight: 25,
+        fontSize: 102,
+        fontWeight: '600',
     },
 
-    outside: {
-        fontStyle: 'normal',
-        fontSize: 23,
-        textAlign: 'center',
-        marginTop: 10,
-        lineHeight: 27
-    },
-
-    date: {
-        fontSize: 23,
-        marginTop: 40,
+    weather: {
+        color: "#fff",
+        fontSize: 48,
+        fontWeight: '600',
         textAlign: 'center'
-    },
-
-    creditBox: {
-        alignItems: 'center',
-        height: 148,
-        width: 319,
-        backgroundColor: '#FFF',
-        borderRadius: 9,
-        marginTop: 100,
-        padding: 15,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 9,
-        },
-        shadowOpacity: 0.48,
-        shadowRadius: 11.95,
-        elevation: 18,
-        borderRadius: 9,
-        backgroundColor: "#fff",
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center'
     },
 
     credit: {
         textAlign: 'center',
-        fontSize: 23,
+        marginTop: 50,
     },
 
-    name: {
-        color: '#0085ff'
+    myName: {
+        fontSize: 24,
+        color: "#fff"
+    },
+
+    fullName: {
+        color: "#45b6fe",
+        fontStyle: 'italic',
+        fontWeight: '600'
+    },
+
+    minMax: {
+        color: "#fff",
+        fontSize: 28,
+        marginRight: 30,
+        marginLeft: 30,
+        marginBottom: 25
     }
 
-
 });
-
