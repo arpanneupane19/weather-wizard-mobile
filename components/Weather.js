f8cd152e788e823dc435f5b9b8ed69465c5fe0a8
Arpan Neupane <arpanneupane19@gmail.com>
2021-02-15 09:37:21 -0600

    added favorites button

--- components/Weather.js
+++ components/Weather.js
@@ -1,29 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, ImageBackground, ScrollView, Text, View, TextInput, TouchableOpacity} from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, ImageBackground, ScrollView, Text, View, TextInput, TouchableOpacity, Button } from "react-native";
+import favorites from "./favorites.json";
 
 
 export default function Weather() {
-    const [weatherData, setWeatherData] = useState([{}])
-    const apiKey = '<API>'
-    const [city, setCity] = useState('');
+    const [weatherData, setWeatherData] = useState([{}]);
+    const apiKey = "<API>";
+    const [city, setCity] = useState("");
     const [unit, setUnit] = useState(true);
 
     const dateBuilder = (d) => {
-        let months = ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sep', "Oct", 'Nov', 'Dec'];
-        let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        let months = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
         let day = days[d.getDay()];
         let date = d.getDate();
         let month = months[d.getMonth()];
         let year = d.getFullYear();
 
-        return `${day} ${month} ${date}, ${year}`
-
-    }
+        return `${day} ${month} ${date}, ${year}`;
+    };
 
     return (
-        <ImageBackground source={(typeof weatherData.main != 'undefined') ? (weatherData.main.temp > 50) ? require('../assets/warm-bg.jpg') : require('../assets/cold-bg.jpg') : require('../assets/cold-bg.jpg')} style={{ width: '100%', height: '105%'}}>
+        <ImageBackground source={(typeof weatherData.main != 'undefined') ? (weatherData.main.temp > 50) ? require('../assets/warm-bg.jpg') : require('../assets/cold-bg.jpg') : require('../assets/cold-bg.jpg')} style={{ width: '100%', height: '105%' }}>
             <ScrollView style={styles.container}>
                 <View>
                     <View style={styles.main}>
@@ -53,125 +53,129 @@ export default function Weather() {
                                 <TouchableOpacity style={styles.weatherBox} onPress={() => unit ? setUnit(false) : setUnit(true)}>
                                     {unit ? <Text style={styles.temp}>{Math.round(weatherData.main.temp)}ºF</Text> : <Text style={styles.temp}>{Math.round((weatherData.main.temp - 32) * 5 / 9)}ºC</Text>}
                                 </TouchableOpacity>
-                                    {unit ? <Text style={styles.minMax}>Min: {Math.round(weatherData.main.temp_min)}ºF / Max: {Math.round(weatherData.main.temp_max)}ºF</Text> : <Text style={styles.minMax}>Min: {Math.round((weatherData.main.temp_min - 32) * 5/9)}ºC / Max: {Math.round((weatherData.main.temp_max - 32) * 5/9)}ºC</Text>}
+                                {unit ? <Text style={styles.minMax}>Min: {Math.round(weatherData.main.temp_min)}ºF / Max: {Math.round(weatherData.main.temp_max)}ºF</Text> : <Text style={styles.minMax}>Min: {Math.round((weatherData.main.temp_min - 32) * 5 / 9)}ºC / Max: {Math.round((weatherData.main.temp_max - 32) * 5 / 9)}ºC</Text>}
                                 <Text style={styles.weather}>{weatherData.weather[0].main}</Text>
 
                             </View>
                         ) : (<>
-                                <View style={styles.weatherInfo}>
-                                    <Text style={styles.city}>Weather Wizard</Text>
-                                    <Text style={styles.date}>{dateBuilder(new Date())}</Text>
-                                </View>
-
-                            </>)}
-
-                            {(weatherData.cod === '404') ? (
-                                <Text style={{marginTop: 50, color: '#fff', fontSize: 20, textAlign: 'center', marginRight: 20, marginLeft: 20}}>No city found. Maybe try just putting the city name?</Text>
-                            ): (<></>)}
-
-                            <View style={styles.credit}>
-                                <Text style={styles.myName}>Developed by <Text style={styles.fullName}>Arpan Neupane</Text></Text>
+                            <View style={styles.weatherInfo}>
+                                <Text style={styles.city}>Weather Wizard</Text>
+                                <Text style={styles.date}>{dateBuilder(new Date())}</Text>
                             </View>
-                            
-                        
-                        
+                        </>)}
+
+                        <Button
+                            title="Add to favorites"
+                            onPress={() => console.log("hello")}
+                        />
+
+                        {(weatherData.cod === '404') ? (
+                            <Text style={{ marginTop: 50, color: '#fff', fontSize: 20, textAlign: 'center', marginRight: 20, marginLeft: 20 }}>No city found. Maybe try just putting the city name?</Text>
+                        ) : (<></>)}
+
+                        <View style={styles.credit}>
+                            <Text style={styles.myName}>Developed by <Text style={styles.fullName}>Arpan Neupane</Text></Text>
+                        </View>
+
+
+
                     </View>
 
 
                 </View>
             </ScrollView>
         </ImageBackground>
-
     );
 }
 
-
 const styles = StyleSheet.create({
-
     container: {
-
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
     },
 
     main: {
         marginTop: 66,
-        width: '100%',
+        width: "100%",
+
         flex: 1,
-        alignItems: 'center'
+        alignItems: "center",
     },
 
     cityInput: {
-        width: '85%',
-        marginRight: '10%',
-        marginLeft: '10%',
+        width: "85%",
+        marginRight: "10%",
+        marginLeft: "10%",
         height: 54,
         padding: 15,
-        backgroundColor: 'rgba(255,255,255,0.8)',
-        color: '#000',
-        fontWeight: '400',
+        backgroundColor: "rgba(255,255,255,0.8)",
+        color: "#000",
+        fontWeight: "400",
         borderRadius: 7,
-        fontSize: 20
+        fontSize: 20,
     },
 
     weatherInfo: {
-        marginTop: 100
+        marginTop: 100,
     },
 
     city: {
-        color: '#fff',
+        color: "#fff",
         fontSize: 32,
-        fontWeight: '300',
-        textAlign: 'center',
+        fontWeight: "300",
+        textAlign: "center",
     },
 
     date: {
         color: "#fff",
         fontSize: 20,
-        fontWeight: '300',
-        fontStyle: 'italic',
-        textAlign: 'center'
+        fontWeight: "300",
+        fontStyle: "italic",
+        textAlign: "center",
     },
 
     weatherBox: {
-        textAlign: 'center'
+        textAlign: "center",
     },
 
     temp: {
-        position: 'relative',
+        position: "relative",
         marginTop: 30,
         marginBottom: 30,
-        backgroundColor: 'rgba(255,255,255,0.2)',
-        marginRight: 'auto',
-        marginLeft: 'auto',
+        backgroundColor: "rgba(255,255,255,0.2)",
+        marginRight: "auto",
+        marginLeft: "auto",
         color: "#fff",
         paddingTop: 15,
         paddingBottom: 15,
         paddingLeft: 25,
         paddingRight: 25,
         fontSize: 102,
-        fontWeight: '600',
+        fontWeight: "600",
     },
 
     weather: {
         color: "#fff",
         fontSize: 48,
-        fontWeight: '600',
-        textAlign: 'center'
+        fontWeight: "600",
+        textAlign: "center",
     },
 
     credit: {
-        textAlign: 'center',
+        textAlign: "center",
         marginTop: 50,
     },
 
     myName: {
         fontSize: 24,
-        color: "#fff"
+        color: "#fff",
     },
 
     fullName: {
         color: "#45b6fe",
-        fontStyle: 'italic',
-        fontWeight: '600'
+        fontStyle: "italic",
+        fontWeight: "600",
     },
 
     minMax: {
@@ -179,7 +183,7 @@ const styles = StyleSheet.create({
         fontSize: 28,
         marginRight: 30,
         marginLeft: 30,
-        marginBottom: 25
-    }
-
+        marginBottom: 25,
+    },
 });
+
